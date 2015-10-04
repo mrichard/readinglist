5a0689f1cf805500da84e1e996ca702a9a8e17ef
Michel Richard <mrichard4@lm3000026417.home>
2015-10-04 12:16:29 -0400

    forms with validation and observers

--- src/readingListForm/readingListForm.ts
+++ src/readingListForm/readingListForm.ts
@@ -5,36 +5,72 @@ import {
 	View
 } from 'angular2/angular2';
 
+import {
+	FormBuilder,
+	FORM_DIRECTIVES,
+	ControlGroup,
+	Control,
+	Validators,
+	NgIf
+} from 'angular2/angular2';
+
 import {Article} from '../models/article';
 
 @Component({
 	selector: 'reading-list-form',
-	properties: ['articles']
+	properties: ['articles'],
+	viewInjector: [FormBuilder]
 })
 
 @View({
+	directives: [FORM_DIRECTIVES, NgIf],
 	template: `
-		<div class="form-group">
-			<div><label for="title">Title:</label></div>
-			<div><input type="text" name="title" class="form-control" #newtitle /></div>
-		</div>
-		<div class="form-group">
-			<div><label for="link">Link:</label></div>
-			<div><input type="text" name="link" class="form-control" #newlink /></div>
-		</div>
-		<button class="btn btn-primary" (click)="addArticle(newtitle, newlink)" >Submit Article</button>
+		<form [ng-form-model]="myForm" (submit)="onSubmit(myForm.value)">
+			<div class="form-group" 
+				[class.has-error]="! myForm.controls.title.valid && myForm.controls.title.touched"
+				[class.has-success]="myForm.controls.title.valid && myForm.controls.title.touched"
+			>
+				<div><label for="title">Title:</label></div>
+				<div><input type="text" id="title" name="title" class="form-control" placeholder="Enter a title" [ng-form-control]="myForm.controls.title" /></div>
+
+				<div *ng-if="! myForm.controls.title.valid && myForm.controls.title.touched">Title is invalid</div>
+				<div *ng-if="myForm.controls.title.hasError('required') && myForm.controls.title.touched">Title is required</div>
+			</div>
+			<div class="form-group" 
+				[class.has-error]="! myForm.controls.link.valid && myForm.controls.link.touched"
+				[class.has-success]="myForm.controls.link.valid && myForm.controls.link.touched"
+			>
+				<div><label for="link">Link:</label></div>
+				<div><input type="text" id="link" name="link" class="form-control" placeholder="Enter a link" [ng-form-control]="myForm.controls.link" /></div>
+				<div *ng-if="! myForm.controls.link.valid && myForm.controls.link.touched">Link is invalid</div>
+				<div *ng-if="myForm.controls.link.hasError('required') && myForm.controls.link.touched">Link is required</div>
+			</div>
+			<button class="btn btn-primary" type="submit">Submit Article</button>
+		</form>
 	`
 })
 
 export class ReadingListForm {
+	myForm: ControlGroup;
 	articles: Array<Article>;
 
-	addArticle(title, link) {
-		console.log(title.value, link.value);
-		this.articles.push( new Article(title.value, link.value, 0));
-		console.log(this.articles);
-		title.value = '';
-		link.value = '';
-		return false; 
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			title: ['', Validators.required],
+			link: ['', Validators.required]
+		});
+
+		this.myForm.valueChanges.observer({
+			next: (value) => {
+				console.log(value);
+			}
+		});
+	}
+
+	onSubmit(value) {
+		if (this.myForm.valid) {
+			this.articles.push( new Article(value.title, value.link, 0));
+			console.log(this.myForm);
+		}
 	}
 }
